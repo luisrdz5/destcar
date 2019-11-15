c778eceb424ce786d066dd24223d0cd8f699f5b0
luisrdz5 <luis@luisrodriguez.com>
2019-11-15 01:28:15 -0600

    (backend) added the maps API

--- backend/src/lib/maps.js
+++ backend/src/lib/maps.js
@@ -1,7 +1,10 @@
 const { config } = require('../config');
 const axios= require('axios');
+const MongoLib = require('../lib/mongo');
+//const DestcarService = require('../services/destcar');
 
 const APIKey= config.googleAPIKey;
+const country= config.country;
 
 // Disneyland y Universal+Studios+Hollywood
 class mapsAPI  {
@@ -11,40 +14,54 @@ class mapsAPI  {
         this.destination = `&destination=`;
         this.key = `&key=${APIKey}`;
         this.dinamicrate = 1;
+        this.mongoDB = new MongoLib();
     }
     async getAmount({ route }){
         const {origin, destination} = route
-    try{
-        //console.log(origin.replace(/\s/g,"+"));
-        let URI = this.MapsURI+this.origin+encodeURIComponent(origin.replace(/\s/g,"+"))+this.destination+encodeURIComponent(destination.replace(/\s/g,"+"))+this.key;
-        let info = await axios({
-            url: URI,
-            method: 'get',
-            responseEncoding: 'utf8',
-        });
-        info = info.data
-        let arrayLegs;
-        info.routes.map((item) => {
-            arrayLegs = item.legs;
-        });
-        let distance;
-        let time;
-        arrayLegs.map((item) => {
-            distance=item.distance.value;
-            time=item.duration.value;
-        })
-        console.log(distance);
-        console.log(time);
-        return arrayLegs;
+        let costByKM;
+        let costByMinute;
+        let minPayment; 
+        let baseRate;
+        try{
+            const query = country && { country };
+            const quotes = await this.mongoDB.getAll('quotes',query);
+            quotes.map((item) => {
+                costByKM=parseFloat(item.costByKM);
+                costByMinute=parseFloat(item.costByMinute);
+                minPayment=parseFloat(item.minPayment);
+                baseRate=parseFloat(item.base);
+            })
+        }catch(err){
+            console.log(err);
+        }
+        try{
+            //console.log(origin.replace(/\s/g,"+"));
+            let URI = this.MapsURI+this.origin+encodeURIComponent(origin.replace(/\s/g,"+"))+this.destination+encodeURIComponent(destination.replace(/\s/g,"+"))+this.key;
+            let info = await axios({
+                url: URI,
+                method: 'get',
+                responseEncoding: 'utf8',
+            });
+            info = info.data
+            let distance;
+            let time;
+            info.routes.map((payload) => {
+                payload.legs.map((item) => {
+                    distance=parseFloat(item.distance.value);
+                    time=parseFloat(item.duration.value);
+                })
+            });
+            //calculating the rate 
+            let total = baseRate + ((distance / 1000) * costByKM)+((time / 60) * costByMinute); 
+            if(total<minPayment){
+                total=minPayment
+            }
+            return total;
 
-    }catch(err){
-        console.log(err);
+        }catch(err){
+            console.log(err);
+        }    
     }
-
-      
-    }
-
-    
 }
 
 module.exports = mapsAPI;
