e06bcabf7a11e17dc9ef29a10239accb98859b1e
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-11-20 10:51:21 -0600

    (frontend) added routes to our project

--- frontend/src/routes/App.js
+++ frontend/src/routes/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Route }  from 'react-router-dom';
-import Home from '../containers/Home'
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "../containers/Home";
 
 const App = () => {
-
-    return (
-        <BrowserRouter>
-            <Route exact path="/" component={Home} />
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+    </BrowserRouter>
+  );
 };
 
 export default App;
